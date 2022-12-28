e3bd8517575b3d416a2cf44a8943b85ad5a9fafe
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-12-28 13:40:23 +0530

    Server Instantiation

---
+++ greet/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/ShyamSundhar1411/Simple-gRPC-Go/greet/proto"
+	"google.golang.org/grpc"
+)
+var address string = "0.0.0.0:50051"
+type Server struct{
+	pb.GreetServiceServer
+}
+func main(){
+	listener,err := net.Listen("tcp",address)
+	if err!=nil{
+		log.Fatalf("Failed to listen on: %v\n",err)
+	}
+	log.Printf("Listening on %s\n",address)
+
+	serverInstance := grpc.NewServer()
+	if err = serverInstance.Serve(listener); err !=nil{
+		log.Fatalf("Failed to serve: %v\n",err)
+	}
+}
